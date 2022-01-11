63f6a7d17323ae9d1d341af4bee58ffe1212f385
rajitechis <rajit@techis.io>
2022-01-11 12:27:42 +0530

    first

---
+++ frontend/src/components/Common/About.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function About() {
+  return (
+    <>
+      <div class="heading-para">
+        <h1>About Us</h1>
+        <p>
+          In Dino’s Pizzeria pizza usually falls into two categories:
+          <br />
+          thick and cheesy Chicago style or thin and more traditional Dino’s
+          Pizzeria pizza.
+          <br />
+          In Italy pizza also falls into two distinct categories: Italian pizza
+          and the rest of the world.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default About;
